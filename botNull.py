ecba3972924c211ed0acdc7e3abd49182900703d
osdanova <osdanovaonline@outlook.com>
2020-11-10 19:31:57 +0100

    Comando capis y funcion searchString
    Añadido un comando para los capis y una funcion para buscar strings en el mensaje.

--- botNull.py
+++ botNull.py
@@ -59,13 +59,14 @@ def echo(bot, update):
     global muchoTexto 
     global timeTexto 
 
-
+    mensaje = update.message
+    mensajeLower = update.message.text.lower()
 
     #if (datetime.now() - timeTexto).total_seconds() < 60 and muchoTexto > 20:
     #   bot.sendDocument(chat_id=update.message.chat_id, document=open(dataPath + '/photo/texto.webp', 'rb'))
 
     #pajero
-    if datetime.now().hour == 22 and datetime.now().minute == 35  and re.search(r'\bhora patito\b',update.message.text.lower()):
+    if datetime.now().hour == 22 and datetime.now().minute == 35  and searchString(mensajeLower, 'hora patito')):
         update.message.reply_text('Felicidades')
 
     #basicos
@@ -75,41 +76,61 @@ def echo(bot, update):
         restart(bot, update)
 
     #null
-    if re.search(r'\bpaja\b', update.message.text.lower()):
+    if searchString(mensajeLower, 'paja'):
         now = datetime.now()
         if now.hour < 18 and now.hour > 8  and datetime.today().weekday() <= 4  :
             update.message.reply_text('Deja de hacerte pajas ' + update.message.from_user.name + ' en el trabajo')
         else:
             update.message.reply_text('Deja de hacerte pajas ' + update.message.from_user.name)
+
     elif re.search(r'\bbuenos d[ií]as\b', update.message.text.lower()):
         bot.sendDocument(chat_id=update.message.chat_id, document=open(dataPath + '/photo/buenos.webp', 'rb'))
-    elif re.search(r'\btop comentarios\b',update.message.text.lower()):
+
+    elif searchString(update, 'top comentarios'):
         update.message.reply_text('''- Joe, no hay gow?
 - Me siento engañado, pense k Joseph era el de la gorra 
 - Donde Eren mate a Levi 
 - LO HIZE! 
 - ¿Para qué voy a chuparlo? si es para jugar
 - Akainu se parece a Luis Augusto
-         ''')     
+         ''')   
+
     elif update.message.text.lower()[-3:] == 'ps5':
         update.message.reply_text('POR EL CULO TE LA HINCO')
-    elif re.search(r'\bxd\b',update.message.text.lower()) and randint(0,10) == 1 :
+
+    elif searchString(mensajeLower, xd') and randint(0,10) == 1 :
         bot.sendDocument(chat_id=update.message.chat_id, document=open(dataPath + '/gifs/salvame.mp4', 'rb'))
+
     elif re.search(r'\bnuevos cap[ií]tulos\b',update.message.text.lower()):
         rows = postgre.select("select * from new_cap order by id limit 5")
         for row in rows:
             bot.send_message(chat_id=update.message.chat_id, text='Titulo: '+row[0]+ ' Cap: '+ str(row[1])+' Url: '+row[2])
-    elif re.search(r'\bbusca anime\b',update.message.text.lower()):
+
+    elif searchString(mensajeLower, 'busca anime'):
         anime = update.message.text.lower().replace("busca anime", "").strip()
         rows = postgre.select("select * from anime where lower(titulo) like '"+anime+"%' limit 10")
         for row in rows:
             bot.send_message(chat_id=update.message.chat_id, text='Resultados: '+ row[0])
-    elif re.search(r'\bbusca cap\b',update.message.text.lower()):
+
+    elif searchString(mensajeLower, 'busca cap'):
         cap = update.message.text.lower().replace("busca cap", "").strip().split(" ", 1)
         rows = postgre.select("select * from capitulos where lower(anime) = '"+cap[1]+"' and episodio ="+cap[0])
         for row in rows:
             bot.send_message(chat_id=update.message.chat_id, text='Titulo: '+row[0]+ ' Cap: '+ str(row[1])+' Url: '+row[2])
 
+# Devuelve true si msg contiene str
+def searchString(msg, str):
+    return re.search(r'\b{re.escape(str)}\b', msg)
+
+# Imprime capis de anime nuevos
+def animeCaps(bot, update, cantidad):
+    capis = postgre.select("select * from new_cap order by id limit " + cantidad)
+    for capi in capis:
+        printAnime(bot, update, row[0], str(row[1]), row[2])
+
+# Imprime un mensaje de un capi formateado
+def printAnime(bot, update, titulo, capi, url):
+    bot.send_message(chat_id=update.message.chat_id, parse_mode='HTML', text="""<b>titulo</b><br>capi<br>url""")
 
 def isAdmin(bot, update):
     if str(update.message.from_user.id) == str(settings["main"]["admin"]) :
@@ -117,8 +138,10 @@ def isAdmin(bot, update):
     else:
         return None
 
-
-
+# COMANDO: imprime 5 capis nuevos
+def cmd_capis(bot, update):
+    animeCaps(bot, update, 5)
+      
 def error(bot, update, error):
     logger.warn('Update "%s" caused error "%s"' % (update, error))
 
@@ -147,6 +170,7 @@ def main():
     dp.add_handler(CommandHandler("help", help))
     dp.add_handler(CommandHandler('seguir', restart))
     dp.add_handler(CommandHandler('parar', stop))
+    dp.add_handler(CommandHandler("Capis", cmd_capis))
 
     dp.add_handler(MessageHandler(Filters.text, echo))
 
